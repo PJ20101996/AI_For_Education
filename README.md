b0be0d1aa95747996859ac9f900528f89d1d7fcc
PJ20101996 <jpagolu@mitresources.com>
2025-11-12 16:22:31 +0530

    Add back_end and front_end folders

--- README.md
+++
@@ -1,2 +0,0 @@
-# AI_For_Education
-Building a POC for Students 
--- db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-# Replace with your MongoDB URI
-MONGO_URI = "mongodb://localhost:27017/"
-client = MongoClient(MONGO_URI)
-
-db = client["ai_summarizer"]       # Database name
-users_collection = db["users"]     # Collection (table)
